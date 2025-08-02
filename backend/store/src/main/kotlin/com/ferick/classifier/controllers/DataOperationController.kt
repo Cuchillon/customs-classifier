72af862601acaeee7d311be534018244188ff0a6
Cuchillon <ferickadmin@gmail.com>
2025-08-02 18:25:34 +0300

    Adding docker

--- backend/store/src/main/kotlin/com/ferick/classifier/controllers/DataOperationController.kt
+++ backend/store/src/main/kotlin/com/ferick/classifier/controllers/DataOperationController.kt
@@ -4,18 +4,21 @@ import com.ferick.classifier.model.dto.StoreMeta
 import com.ferick.classifier.model.dto.StoreRequest
 import com.ferick.classifier.model.dto.UserSearchRequest
 import com.ferick.classifier.service.DataOperationService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 class DataOperationController(
     private val dataOperationService: DataOperationService
 ) {
 
-    @PostMapping("/store")
+    @PostMapping("/store", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun storeData(
         @RequestPart meta: StoreMeta,
         @RequestPart data: MultipartFile
